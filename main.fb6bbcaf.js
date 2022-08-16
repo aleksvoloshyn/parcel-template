472b7762353bf0b24ef019082930adce4469098e
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2022-08-16 11:40:20 +0300

    Updates

--- main.fb6bbcaf.js
+++ main.fb6bbcaf.js
@@ -139,7 +139,7 @@ var _test = require("./test");
 // console.log(_.concat([10, 20, 30], [50]))
 console.log('hi, i am your empty parcel project by ALex');
 (0, _test.logger)('Alex');
-},{"./test":"js/test.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./test":"js/test.js"}],"C:/Users/kornu/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -167,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53456" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54180" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -343,5 +343,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/main.js"], null)
+},{}]},{},["C:/Users/kornu/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/main.js"], null)
 //# sourceMappingURL=/main.fb6bbcaf.js.map
