d0fdbf764058158d8029521001b4b30619dc0b83
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2022-08-16 11:26:12 +0300

    config modified

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 
 import { logger as greeting } from './test'
 
-console.log(_.concat([10, 20, 30], [50]))
+// console.log(_.concat([10, 20, 30], [50]))
 
 console.log('hi, i am your empty parcel project by ALex')
 
